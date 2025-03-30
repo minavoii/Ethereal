ac16c2b78b2bbbe4c50410a3ea362e1cb3daca53
Minavoii <146578755+minavoii@users.noreply.github.com>
2025-03-30 17:46:42 +0200

    Examples: add Localisation API example

---
+++ examples/localisations/Text/Apple.cs
@@ -0,0 +1,16 @@
+namespace Localisations.Text;
+
+internal static class Apple
+{
+    internal const int Id = 9901;
+
+    internal const string Original = "Apple";
+
+    internal static readonly LocalisationData.LocalisationDataEntry entry = new()
+    {
+        ID = Id,
+        StringContent = Original,
+        StringContentEnglish = Original,
+        StringContentFrench = "Pomme",
+    };
+}
