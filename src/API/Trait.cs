e4dc16bab4c91dc8545bf80f016d2edfac0fe909
Minavoii <146578755+minavoii@users.noreply.github.com>
2025-04-01 21:23:01 +0200

    API: add Trait.Update method

--- src/API/Trait.cs
+++ src/API/Trait.cs
@@ -1,9 +1,81 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Ethereal.API;
 
 public static class Trait
 {
+    public class TraitDescriptor()
+    {
+        public string name = "";
+
+        public string description = "";
+
+        public string sidenote = "";
+
+        public bool? aura;
+
+        public bool? maverickSkill;
+
+        public List<PassiveEffect> passiveEffects = [];
+
+        public List<EMonsterType> types = [];
+
+        public Sprite icon;
+
+        public ESkillType? skillType;
+    }
+
+    private static readonly ConcurrentQueue<(int id, TraitDescriptor descriptor)> QueueUpdate =
+        new();
+
+    private static readonly ConcurrentQueue<(
+        string name,
+        TraitDescriptor descriptor
+    )> QueueUpdateByName = new();
+
+    private static bool IsReady = false;
+
+    internal static void ReadQueue()
+    {
+        IsReady = true;
+
+        while (QueueUpdate.TryDequeue(out var res))
+            Update(res.id, res.descriptor);
+
+        while (QueueUpdateByName.TryDequeue(out var res))
+            Update(res.name, res.descriptor);
+    }
+
+    public static global::Trait Get(int id)
+    {
+        // Find trait by monster type
+        foreach (MonsterType type in GameController.Instance.MonsterTypes)
+        {
+            global::Trait trait = type.Traits.Find(x => x.ID == id);
+
+            if (trait != null)
+                return trait;
+        }
+
+        // Find signature trait
+        foreach (GameObject monster in GameController.Instance.CompleteMonsterList)
+        {
+            if (monster == null)
+                continue;
+
+            global::Trait trait = monster
+                .GetComponent<SkillManager>()
+                ?.SignatureTrait?.GetComponent<global::Trait>();
+
+            if (trait.ID == id)
+                return trait;
+        }
+
+        return null;
+    }
+
     public static global::Trait Get(string name)
     {
         // Find trait by monster type
@@ -30,5 +102,85 @@ public static class Trait
         }
 
         return null;
+    }
+
+    public static void Update(int id, TraitDescriptor descriptor)
+    {
+        // Defer loading until ready
+        if (GameController.Instance?.CompleteMonsterList == null || !IsReady)
+        {
+            QueueUpdate.Enqueue((id, descriptor));
+            return;
+        }
+
+        Update(Get(id), descriptor);
+    }
+
+    public static void Update(string name, TraitDescriptor descriptor)
+    {
+        // Defer loading until ready
+        if (GameController.Instance?.CompleteMonsterList == null || !IsReady)
+        {
+            QueueUpdateByName.Enqueue((name, descriptor));
+            return;
+        }
+
+        Update(Get(name), descriptor);
+    }
+
+    private static void Update(global::Trait trait, TraitDescriptor descriptor)
+    {
+        if (descriptor.name != string.Empty)
+            trait.Name = descriptor.name;
+
+        if (descriptor.description != string.Empty)
+            trait.Description = descriptor.description;
+
+        if (descriptor.sidenote != string.Empty)
+            trait.Sidenote = descriptor.sidenote;
+
+        if (descriptor.aura.HasValue)
+            trait.Aura = descriptor.aura.Value;
+
+        if (descriptor.maverickSkill.HasValue)
+            trait.MaverickSkill = descriptor.maverickSkill.Value;
+
+        if (descriptor.passiveEffects.Count != 0)
+        {
+            foreach (PassiveEffect comp in trait.GetComponents<PassiveEffect>())
+            {
+                Object.DestroyImmediate(comp);
+            }
+
+            GameObject go = trait.gameObject;
+
+            foreach (PassiveEffect passive in descriptor.passiveEffects)
+            {
+                Utils.Converter.CopyToGameObject(ref go, passive);
+            }
+
+            trait.InitializeReferenceable();
+        }
+
+        if (descriptor.types.Count != 0)
+        {
+            trait.Types.Clear();
+
+            foreach (var monsterType in descriptor.types)
+            {
+                MonsterType type = GameController.Instance.MonsterTypes.Find(x =>
+                    x?.Type == monsterType
+                );
+                GameObject go = Utils.Converter.IntoGameObject(type);
+
+                trait.Types.Add(go);
+            }
+        }
+
+        if (descriptor.icon != null)
+            trait.Icon = descriptor.icon;
+
+        if (descriptor.skillType.HasValue)
+            trait.SkillType = descriptor.skillType.Value;
     }
 }
