815025a106af52702b9036e94582b497f935e2cf
Minavoii <146578755+minavoii@users.noreply.github.com>
2025-10-27 20:27:31 +0100

    API: add the `Animations` and `Textures` APIs

---
+++ src/API/Textures.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using UnityEngine;
+
+namespace Ethereal.API;
+
+public static partial class Textures
+{
+    /// <summary>
+    /// Create a texture from an image file.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="asset"></param>
+    /// <returns>a Texture2D if the file was found; otherwise null.</returns>
+    public static Texture2D? LoadFromImage(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Log.API.LogWarning("File not found: " + path);
+            return null;
+        }
+
+        Texture2D texture = new(0, 0);
+        texture.LoadImage(File.ReadAllBytes(path));
+
+        return texture;
+    }
+}
