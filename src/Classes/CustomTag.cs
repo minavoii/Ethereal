290a8a9c6be6887aaf6f15d3ab747cc250d26b08
Pepper Muangmingsuk <33099029+HLXII@users.noreply.github.com>
2025-12-04 17:21:10 -0600

    Add support for scoped custom objects

--- src/Classes/CustomTag.cs
+++ src/Classes/CustomTag.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 
 namespace Ethereal.CustomFlags;
 
-public sealed class CustomTagComponent : MonoBehaviour { }
+public sealed class CustomTagComponent : MonoBehaviour
+{
+    public string? Scope { get; set; }
+}
 
 public static class GameObjectExtensions
 {
+    public static void AddCustomTag(this GameObject go, string? scope)
+    {
+        CustomTagComponent tag = go.AddComponent<CustomTagComponent>();
+        tag.Scope = API.Scopes.CurrentScope;
+    }
+
     public static bool IsCustomObject(this GameObject go)
     {
         return go?.GetComponent<CustomTagComponent>() != null;
